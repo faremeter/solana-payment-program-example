8898479666aa14f1eef77bb9162a18bcbf52226e
FarinM <farin681@gmail.com>
2025-06-27 23:53:40 +0200

    Update node server to return the admin pubkey

--- examples/node-server/src/types.ts
+++ examples/node-server/src/types.ts
@@ -3,6 +3,7 @@ import { BN } from "@coral-xyz/anchor";
 
 export interface PaymentRequirements {
   receiver: PublicKey;
+  admin: PublicKey;
   amount: number;
 }
 
@@ -29,4 +30,10 @@ export interface TransactionVerificationResult {
 export interface CreatePaymentArgs {
   amount: BN;
   nonce: number[];
+}
+
+export interface PaymentResponse {
+  address: string;
+  admin: string;
+  amount: string;
 }
